879b6df665aeef4e6d65e1e047c7f1c476e37bcb
Alex Schwartz <aschwartz2018@fau.edu>
2020-10-14 09:46:32 -0400

    Commenting MainActivity.java

--- HW6/part4/app/src/main/java/com/alexschwartz/hw6calculator/MainActivity.java
+++ HW6/part4/app/src/main/java/com/alexschwartz/hw6calculator/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Accesses data to/from 2 inputs, operator, equals button, and answer field
         final EditText firstField = findViewById(R.id.input1);
         final Button operator = findViewById(R.id.operator);
         final EditText secondField = findViewById(R.id.input2);
@@ -26,11 +27,18 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //When equals button is pressed do the following
+
+                //Error checking on the inputs (if their length is 0, give error)
                 if(firstField.getText().length()==0 || secondField.getText().length()==0) {
                     answer.setText("Missing input(s)");
-                } else {
+                } else { //If both inputs (forced to be numbers) exist
+
+                    //Gets the float numbers from inputs
                     float num1 = Float.parseFloat(firstField.getText().toString());
                     float num2 = Float.parseFloat(secondField.getText().toString());
+
+                    //Checking the text of the operator button (not the pull-down menu)
                     if(operator.getText()=="Add") {
                         answer.setText(Float.toString(num1 + num2));
                     } else if(operator.getText()=="Subtract") {
@@ -38,12 +46,15 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                     } else if(operator.getText()=="Multiply") {
                         answer.setText(Float.toString(num1 * num2));
                     } else if(operator.getText()=="Divide") {
+                        //Error checking on the second input
                         if(num2==0) {
                             answer.setText("Undefined");
                         } else {
                             answer.setText(Float.toString(num1 / num2));
                         }
                     } else {
+                        //If the operator button text is "Operator" than user
+                        //hasn't selected an operator
                         answer.setText("Missing operator");
                     }
                 }
@@ -51,15 +62,25 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         });
     }
 
+    //When the operator button is clicked it goes here
+    //In main activity XML file: android:onClick="operatorMenu"
+    //in the operator button's tag
     public void operatorMenu(View view) {
         PopupMenu opMenu = new PopupMenu(this, view);
+
+        //Goes to the onMenuItemClick method below
         opMenu.setOnMenuItemClickListener(this);
+
+        //It takes the layout from app/res/menu/operator_popup.xml
+        //and loads it under the operator button
         opMenu.inflate(R.menu.operator_popup);
         opMenu.show();
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
+        //Gets data from operator button
+        //If true, menu item is pressed
         Button operatorText=findViewById(R.id.operator);
         if(item.getItemId()==R.id.addition) {
             operatorText.setText("Add");
