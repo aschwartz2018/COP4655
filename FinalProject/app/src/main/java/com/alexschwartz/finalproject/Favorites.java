0967feec70a3cf1511ffff8e6066dedf795f4c55
Alex Schwartz <aschwartz2018@fau.edu>
2020-11-13 09:04:19 -0500

    Start Final Project - adds login to Google and once user is logged in, app displays name, email, and account photo

---
+++ FinalProject/app/src/main/java/com/alexschwartz/finalproject/Favorites.java
@@ -0,0 +1,44 @@
+package com.alexschwartz.finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Favorites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.getMenu().findItem(R.id.action_favorites).setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(mainActivity);
+                        break;
+                    case R.id.action_search:
+                        Intent results = new Intent(getApplicationContext(), Search.class);
+                        startActivity(results);
+                        break;
+                    case R.id.action_results:
+                        Intent map = new Intent(getApplicationContext(), Results.class);
+                        startActivity(map);
+                        break;
+                    case R.id.action_favorites:
+                        Intent history = new Intent(getApplicationContext(), Favorites.class);
+                        startActivity(history);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
